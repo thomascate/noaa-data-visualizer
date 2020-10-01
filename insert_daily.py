53a48410287826c3a3cb442fd3eb21824a75f3ae
Thomas Cate <thomascate@gmail.com>
2020-10-01 16:20:33 -0500

    first pass at multithreaded data import

--- insert_daily.py
+++ insert_daily.py
@@ -10,6 +10,7 @@ import logging
 import time
 import yaml
 import threading
+import queue
 
 logging.basicConfig(filename='insert_daily.log',level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 configFile = "config.yaml"
@@ -38,7 +39,7 @@ config['elasticPass'] = os.environ.get('NOAA_ELASTIC_PASSWORD')
 if config['sslVerify'] is False:
   requests.packages.urllib3.disable_warnings()
 
-files = glob.glob( config['ghcndLocation'] + "/*.csv")
+config['threads'] = 4
 
 # This is used to verify the source data type, so we can make sure it makes it to the correct type before being dumped to json.
 def contains_int(n):
@@ -57,14 +58,14 @@ def contains_float(n):
   else:
     return True
 
-def insert_data(config, postChunk, docNumber):
+def insert_data(config, postChunk, docNumber, fileName):
   for attempt in range(10):
     print(f"Trying attempt {attempt}")
     try:
       time.sleep(attempt ** 2)
       r = requests.post(config['elasticURL'] + "/_bulk", verify=config['sslVerify'], auth=(config['elasticUser'], config['elasticPass']), data=postChunk, headers=config['elasticHeader'])
       docCount = postChunk.count('\n')/2
-      logging.info(f"docs: {docCount}, docNumber: {docNumber} code: {r.status_code}, attempt: {attempt}")
+      logging.info(f"docs: {docCount}, docNumber: {docNumber} code: {r.status_code}, fileName: {fileName}, attempt: {attempt}")
 
       if not r.status_code == 200:
         raise Exception(f"insert failed code: {r.status_code}")
@@ -76,32 +77,67 @@ def insert_data(config, postChunk, docNumber):
       print(f"try failed {r.status_code}")
       return
 
-# We need to track all fields that return in 10ths of a degree Celcius, so that we can later convert them to C and F.
-celciusFields = [
-  "TAVG",
-  "TMAX",
-  "TMIN",
-  "MDTN",
-  "MDTX",
-  "MNPN",
-  "MXPN",
-  "TOBS"
-]
+def celcius_fields():
+  # We need to track all fields that return in 10ths of a degree Celcius, so that we can later convert them to C and F.
+  celciusFields = [
+    "TAVG",
+    "TMAX",
+    "TMIN",
+    "MDTN",
+    "MDTX",
+    "MNPN",
+    "MXPN",
+    "TOBS"
+  ]
 
-# We need to add key names of S[N,X][0-8][1-7] as the soil type and depth of the measurement is embedded in the key name.
-for i in range(0,9):
-  for j in range(1,8):
-    celciusFields.append("SN"+str(i)+str(j))
-    celciusFields.append("SX"+str(i)+str(j))
+  # We need to add key names of S[N,X][0-8][1-7] as the soil type and depth of the measurement is embedded in the key name.
+  for i in range(0,9):
+    for j in range(1,8):
+      celciusFields.append("SN"+str(i)+str(j))
+      celciusFields.append("SX"+str(i)+str(j))
 
-for file in files:
-  if os.path.exists(file + ".old"):
-    logging.info(f'{file}.old exists, skipping')
-    continue
-  logging.info(file)
-  csvData = csv.DictReader(open(file).readlines())
-  originalData = open(file).read().splitlines()
+  return(celciusFields)
+
+def mm_fields():
+  # These fields are defined in mm, defining here in case we want to save a copy in inches
+  mmFields = [
+    "PRCP",
+    "SNOW",
+    "SNWD",
+    "EVAP",
+    "MDEV",
+    "MDPR",
+    "MDSF",
+    "THIC",
+    "WESF"
+  ]
+
+  return(mmFields)
+
+def cm_fields():
+  # These fields are defined in cm, defining here in case we want to save a copy in inches
+  cmFields = [
+    "FRGB",
+    "FRGT",
+    "FRTH",
+    "FRTH",
+    "GAHT"
+  ]
+
+  return(cmFields)
+
+def generate_document(fileName):
+
+  celciusFields = celcius_fields()
   uploadDocument = ""
+
+  if os.path.exists(fileName + ".done"):
+    logging.info(f'{fileName}.done exists, skipping')
+    return(uploadDocument)    
+
+  logging.info(fileName)
+  csvData = csv.DictReader(open(fileName).readlines())
+  originalData = open(fileName).read().splitlines()
   i = 1
 
   for entry in csvData:
@@ -146,6 +182,8 @@ for file in files:
     # We want docID to be consistent, which means we can run this against duplicated data without getting duplicated entries in ES
     docID = hashlib.sha224(idString.encode('utf-8')).hexdigest()
     indexName = "noaa-data-" + jsonEntry['DATE'].replace('-','.')[0:4]
+
+    # This is building a newline seperated _bulk upload document out of the entries
     uploadDocument+=json.dumps( { "index" : { "_index" : indexName, "_id" : docID } } )
     uploadDocument+="\n"
     uploadDocument+=json.dumps( jsonEntry )
@@ -153,21 +191,69 @@ for file in files:
 
     i = i+1
 
-  postChunk = ""
-  docNumber = 0
-  for line in uploadDocument.splitlines():
-     docNumber = docNumber+1
-     postChunk+=line
-     postChunk+="\n"
-     docCount = postChunk.count('\n')
-     if docCount == 500:
+  return(uploadDocument)
+
+def upload_file():
+  while True:
+    if fileQueue.empty() == True:
+      print("nothing left to do")
+      return 
+
+    fileName = fileQueue.get()
+
+    try:
+      print(f"get upload document for {fileName}")
+      uploadDocument = generate_document(fileName)
+
+      if uploadDocument == "":
+        continue
+
+      postChunk = ""
+      docNumber = 0
+
+      for line in uploadDocument.splitlines():
+        docNumber = docNumber+1
+        postChunk+=line
+        postChunk+="\n"
+        docCount = postChunk.count('\n')
+
+        if docCount == 500:
+          print(f"found {docCount} lines, let's post it")
+          insert_data(config, postChunk, docNumber/2, fileName)
+          postChunk = ""
+
+      if len(postChunk) > 0:
         print(f"found {docCount} lines, let's post it")
-        insert_data(config, postChunk, docNumber/2)
+        insert_data(config, postChunk, docNumber/2, fileName)
         postChunk = ""
 
-  if len(postChunk) > 0:
-    print(f"found {docCount} lines, let's post it")
-    insert_data(config, postChunk, docNumber/2)
-    postChunk = ""
+      open(fileName + ".done", 'a').close()
+   #   attributes = {}
+   #   attributes['path'] = fileName
+   #   attributes['name'] = os.path.basename(fileName)
+   #   attributes['size'] = os.path.getsize(fileName)
+   #   print(attributes)
+
+    except Exception as e:
+      print(f"error on {fileName} {str(e)}")
+
+
+files = glob.glob( config['ghcndLocation'] + "/*.csv")
+
+fileQueue = queue.Queue()
+activeThreads = []
+
+for file in files:
+  fileQueue.put(file)
+
+print(fileQueue.qsize())
+
+for i in range(config['threads']):
+  t = threading.Thread(target=upload_file, args=())
+  activeThreads.append(t)
+  t.start()
+
+for activeThread in activeThreads:
+  activeThread.join()
 
 exit()
